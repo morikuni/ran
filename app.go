9093332368af014992ce9386648911fdd1c8b315
morikuni <taihei.with+git@gmail.com>
2019-01-01 16:48:11 +0900

    Introduce cobra

--- app.go
+++ app.go
@@ -2,9 +2,12 @@ package workflow
 
 import (
 	"context"
-	"flag"
+	"errors"
 	"fmt"
 	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 )
 
 type App struct {
@@ -16,40 +19,58 @@ func NewApp(logger Logger) App {
 }
 
 func (app App) Run(ctx context.Context, args []string, signal <-chan os.Signal) error {
-	fs := flag.NewFlagSet(args[0], flag.ContinueOnError)
-	file := fs.String("f", "workflow.yaml", "file")
-	err := fs.Parse(args[1:])
-	if err != nil {
+	cmd := &cobra.Command{
+		Use: "workflow",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return errors.New("require command")
+		},
+	}
+	cmd.SetArgs(args[1:])
+
+	file := cmd.PersistentFlags().StringP("file", "f", "workflow.yaml", "workflow definition file.")
+
+	// parse --file flag before execute to parse and append commands.
+	if err := cmd.PersistentFlags().Parse(args); err != nil && err != pflag.ErrHelp {
+		cmd.Usage()
 		return err
 	}
 
 	def, err := LoadDefinition(*file)
 	if err != nil {
+		cmd.Usage()
 		return err
 	}
 
-	target := fs.Arg(0)
-	command, ok := def.Commands[target]
-	if !ok {
-		return fmt.Errorf("no such workflow: %s", target)
+	for _, c := range def.Commands {
+		cmd.AddCommand(&cobra.Command{
+			Use: c.Name,
+			RunE: func(cmd *cobra.Command, args []string) error {
+				command, ok := def.Commands[cmd.Use]
+				if !ok {
+					return fmt.Errorf("no such workflow: %s", cmd.Use)
+				}
+
+				supervisor := NewSupervisor(app.logger)
+				dispatcher := NewDispatcher(app.logger)
+
+				var initialRunners []*TaskRunner
+				for _, task := range command.Workflow {
+					tr := NewTaskRunner(task, def.Env, supervisor, dispatcher)
+					if len(task.When) == 0 {
+						initialRunners = append(initialRunners, tr)
+					} else {
+						dispatcher.Register(ctx, tr)
+					}
+				}
+
+				for _, tr := range initialRunners {
+					tr.Run(ctx)
+				}
+
+				return supervisor.Wait()
+			},
+		})
 	}
 
-	supervisor := NewSupervisor(app.logger)
-	dispatcher := NewDispatcher(app.logger)
-
-	var initialRunners []*TaskRunner
-	for _, task := range command.Workflow {
-		tr := NewTaskRunner(task, def.Env, supervisor, dispatcher)
-		if len(task.When) == 0 {
-			initialRunners = append(initialRunners, tr)
-		} else {
-			dispatcher.Register(ctx, tr)
-		}
-	}
-
-	for _, tr := range initialRunners {
-		tr.Run(ctx)
-	}
-
-	return supervisor.Wait()
+	return cmd.Execute()
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/morikuni/workflow
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/spf13/cobra v0.0.4-0.20181127133106-d2d81d9a96e2
+	github.com/spf13/pflag v1.0.3
 	github.com/stretchr/testify v1.2.2
 	golang.org/x/sync v0.0.0-20181108010431-42b317875d0f
 	gopkg.in/yaml.v2 v2.2.1
