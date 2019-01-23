e3c994290dfcc4a3cbd13096c7e46b0d5e348ff4
morikuni <taihei.with+git@gmail.com>
2019-01-23 20:08:26 +0900

    Add prefix to event topic

--- dispatcher.go
+++ dispatcher.go
@@ -14,7 +14,7 @@ func NewDispatcher(logger Logger) *Dispatcher {
 }
 
 func (d *Dispatcher) Receive(ctx context.Context, e Event) {
-	d.logger.Debug(e.Topic)
+	d.logger.Debug("event: %q", e.Topic)
 	for _, receiver := range d.receivers {
 		receiver.Receive(ctx, e)
 	}
